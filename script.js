81ce21e348ca4eeffd225ce24342b938e363577c
Pedro <pedro_psantana@hotmail.com>
2022-10-03 23:02:56 -0300

    teste1

---
+++ script.js
@@ -0,0 +1,32 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function () {
+
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+
+    alert("Perdeu! Para tentar novemete precione a tecla (space)");
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
